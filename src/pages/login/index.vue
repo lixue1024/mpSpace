52a40a28061d346f2f89d41c5426db828d514185
lixue1024 <13621208178@163.com>
2020-05-23 22:09:18 +0800

    新建小程序项目mp-space

---
+++ src/pages/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <van-field :value="value" placeholder="请输入用户名"></van-field>
+        <van-button type="info">按钮</van-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            value: ''
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
