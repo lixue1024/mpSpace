22d0a7460a23485ace559d6dfe3303ac2043f234
lixue1024 <13621208178@163.com>
2020-05-24 00:12:06 +0800

    测试soucetree提交

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <van-field :value="value" placeholder="请输入用户名"></van-field>
-        <van-button type="info">按钮1</van-button>
+        <van-button type="info">按钮1xxx</van-button>
     </div>
 </template>
 
